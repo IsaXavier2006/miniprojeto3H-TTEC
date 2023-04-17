24fb2a55e54e0fb0db6d94ab5d46f76e313aed5c
Isabelly Vitoria Xavier de Souza <rm82944@estudante.fieb.edu.br>
2023-04-17 08:03:50 -0300

    classe java Cliente criada

---
+++ Cliente.java
@@ -0,0 +1,15 @@
+public class Cliente {
+	//Atributo
+	private String nome;
+	//Metodo
+	public void falar(){
+		System.out.print("falei")
+	}
+	// Getter e Setter
+	public String getNome(){
+		return this.nome;
+	}
+	public void setNome(String_nome){
+			this.nome = _nome;
+	}
+}
